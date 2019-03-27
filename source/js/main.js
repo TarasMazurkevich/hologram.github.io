802904a466893a8559ce4c11a94bd5cb87957c41
Taras <tartitan1999@gmail.com>
2019-03-27 04:39:48 +0200

    update v2.1

--- source/js/main.js
+++ source/js/main.js
@@ -211,8 +211,7 @@ product.onclick = function (e) {
 		if(target.classList.contains('buy_close')) {
 			let block = target.parentNode.parentNode.parentNode;
 			product.removeChild(block);
-		} else if(target.tagName === 'BUTTON') {
-
+		} else if(target.tagName === 'BUTTON' && !target.classList.contains('condit-btn')) {
 			let block = target.parentNode.parentNode;
 			let data = {
 				src: block.querySelector('.product_img').src,
@@ -223,79 +222,87 @@ product.onclick = function (e) {
 			open(data);
 
 			return;
+		} else if(target.tagName === 'BUTTON' && target.classList.contains('condit-btn')) {
+			open({src: undefined, title: undefined, price: undefined}, 'condit');
+
+			return;
 		} else {
 			target = target.parentNode;
 		}
 	}
 
-	function open({src, title, price}) {
-		let template = document.createElement('div');
-		template.classList.add('buy');
-		template.innerHTML = `
-			<div class="buy__wrap">
-				<div class="buy__header">
-	                <span class="buy_close">
-	                    <svg version="1.1" class="close" id="close" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 47.971 47.971" xml:space="preserve">
-	                        <g>
-	                            <path d="M28.228,23.986L47.092,5.122c1.172-1.171,1.172-3.071,0-4.242c-1.172-1.172-3.07-1.172-4.242,0L23.986,19.744L5.121,0.88
-	                                c-1.172-1.172-3.07-1.172-4.242,0c-1.172,1.171-1.172,3.071,0,4.242l18.865,18.864L0.879,42.85c-1.172,1.171-1.172,3.071,0,4.242
-	                                C1.465,47.677,2.233,47.97,3,47.97s1.535-0.293,2.121-0.879l18.865-18.864L42.85,47.091c0.586,0.586,1.354,0.879,2.121,0.879
-	                                s1.535-0.293,2.121-0.879c1.172-1.171,1.172-3.071,0-4.242L28.228,23.986z"/>
-	                        </g>
-	                    </svg>
-	                </span>
-	                <h2 class="buy_title">Покупка</h2>
-	            </div>
-	            <div class="buy__article">
-	                <img class="buy_img" src="${src}" alt="">
-	                <div class="buy__desc">
-	                    <div>
-	                        <p><span>Товар:</span>${title}</p>
-	                    </div>
-	                    <div >
-	                        <p><span>Цена:</span>${price}</p>
-	                    </div>
-	                </div>
-	            </div>
-	            <form class="buy__form" name="buy">
-	                <div class="buy__block">
-	                    <div class="buy__row">
-	                        <label for="name">Имя и Фамилия<span>*</span>:</label>
-	                        <input type="text" name="name" id="name">
-	                    </div>
-	                    <div class="buy__row">
-	                        <label for="phone">Контактный номер<span>*</span>:</label>
-	                        <input type="phone" name="phone" id="phone">
-	                    </div>
-	                    <div class="buy__row">
-	                        <label for="email">Электронная почта:</label>
-	                        <input type="email" name="email" id="email">
-	                    </div>
-	                </div>
-	                <input class="buy_submit" name="sub" type="submit" value="Купить">
-	            </form>
-			</div>
-		`;
+	function open({src, title, price}, btn) {
+		if(btn === 'condit') {
+			const block = document.querySelector('.condit');
+			block.classList.toggle('active');
+		} else {
+			let template = document.createElement('div');
+			template.classList.add('buy');
+			template.innerHTML = `
+				<div class="buy__wrap">
+					<div class="buy__header">
+		                <span class="buy_close">
+		                    <svg version="1.1" class="close" id="close" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 47.971 47.971" xml:space="preserve">
+		                        <g>
+		                            <path d="M28.228,23.986L47.092,5.122c1.172-1.171,1.172-3.071,0-4.242c-1.172-1.172-3.07-1.172-4.242,0L23.986,19.744L5.121,0.88
+		                                c-1.172-1.172-3.07-1.172-4.242,0c-1.172,1.171-1.172,3.071,0,4.242l18.865,18.864L0.879,42.85c-1.172,1.171-1.172,3.071,0,4.242
+		                                C1.465,47.677,2.233,47.97,3,47.97s1.535-0.293,2.121-0.879l18.865-18.864L42.85,47.091c0.586,0.586,1.354,0.879,2.121,0.879
+		                                s1.535-0.293,2.121-0.879c1.172-1.171,1.172-3.071,0-4.242L28.228,23.986z"/>
+		                        </g>
+		                    </svg>
+		                </span>
+		                <h2 class="buy_title">Покупка</h2>
+		            </div>
+		            <div class="buy__article">
+		                <img class="buy_img" src="${src}" alt="">
+		                <div class="buy__desc">
+		                    <div>
+		                        <p><span>Товар:</span>${title}</p>
+		                    </div>
+		                    <div >
+		                        <p><span>Цена:</span>${price}</p>
+		                    </div>
+		                </div>
+		            </div>
+		            <form class="buy__form" name="buy">
+		                <div class="buy__block">
+		                    <div class="buy__row">
+		                        <label for="name">Имя и Фамилия<span>*</span>:</label>
+		                        <input type="text" name="name" id="name">
+		                    </div>
+		                    <div class="buy__row">
+		                        <label for="phone">Контактный номер<span>*</span>:</label>
+		                        <input type="phone" name="phone" id="phone">
+		                    </div>
+		                    <div class="buy__row">
+		                        <label for="email">Электронная почта:</label>
+		                        <input type="email" name="email" id="email">
+		                    </div>
+		                </div>
+		                <input class="buy_submit" name="sub" type="submit" value="Купить">
+		            </form>
+				</div>
+			`;
 
-		product.appendChild(template);
+			product.appendChild(template);
 
-		let btn = document.forms.buy.elements.sub;
+			let btn = document.forms.buy.elements.sub;
 
-		let form = document.forms.buy;
+			let form = document.forms.buy;
 
-		form.onsubmit = function (e) {
-			e.preventDefault();
-			let formData = new FormData(form);
-			formData.append('title', title);
-			formData.append('price', price);
+			form.onsubmit = function (e) {
+				e.preventDefault();
+				let formData = new FormData(form);
+				formData.append('title', title);
+				formData.append('price', price);
 
-			fetch('./php/mail.php', {
-			  method: 'POST',
-			  body: formData
-			});
-			alert('Спасибо за покупку! Мы с вами свяжемся в ближайшее время!');
-		}
-		
+				fetch('./php/mail.php', {
+				  method: 'POST',
+				  body: formData
+				});
+				alert('Спасибо за покупку! Мы с вами свяжемся в ближайшее время!');
+			}
+		}		
 	}
 
 
@@ -323,4 +330,63 @@ $('.desc').on('mousemove', function(e){
 });
 // Parallax effect END
 
+// --------------------------------------------------------------
+
+// 9)
+// btn condit START
+const btnConditClose = document.querySelector('.condit_close');
+const footerCondit = document.querySelector('.contacts_btn');
+
+footerCondit.onclick = function () {
+	const block = document.querySelector('.condit');
+	block.classList.toggle('active');
+}
+
+btnConditClose.onclick = function () {
+	const block = document.querySelector('.condit');
+	block.classList.toggle('active');
+}
+// btn condit END
+
 })();
+
+
+// --------------------------------------------------------------
+
+// 10)
+// btn about START
+
+const btnAboutClose = document.querySelector('.about_close');
+const footerAbout = document.querySelector('.about-btn');
+
+footerAbout.onclick = function () {
+	const block = document.querySelector('.about');
+	block.classList.toggle('active');
+}
+
+btnAboutClose.onclick = function () {
+	const block = document.querySelector('.about');
+	block.classList.toggle('active');
+}
+
+// btn about END
+
+// --------------------------------------------------------------
+
+// 11)
+// btn ofer START
+
+const btnOferClose = document.querySelector('.ofer_close');
+const footerOfer = document.querySelector('.ofer-btn');
+
+footerOfer.onclick = function () {
+	const block = document.querySelector('.ofer');
+	block.classList.toggle('active');
+}
+
+btnOferClose.onclick = function () {
+	const block = document.querySelector('.ofer');
+	block.classList.toggle('active');
+}
+
+// btn ofer END
